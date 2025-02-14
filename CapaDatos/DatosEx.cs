bc0582d8cd205f01ab26b974f70fb2d2b676d44e
Jake24012001 <oberinho2401@outlook.com>
2025-02-14 09:07:13 -0500

    Agregar archivos de proyecto.

---
+++ CapaDatos/DatosEx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaDatos
+{
+
+    public class DatosEx : ApplicationException
+    {
+        public DatosEx(string mensaje, Exception original)
+               : base(mensaje, original)
+        {
+
+        }
+        public DatosEx(string mensaje)
+            : base(mensaje)
+        {
+
+        }
+
+    }
+}
+
